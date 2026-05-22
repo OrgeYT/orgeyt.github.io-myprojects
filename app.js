99fdcf988c1aa3683a65f56ef578f102f89b7236
OrgeYT <liamupper911@gmail.com>
2026-05-22 19:25:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const projects = [
     "welcome", "fnftools", "spritesheetmerger", "3danimator", "catmemory", 
     "midiplayer", "mandelbrot", "gswitch", "pfpmaker", "platformer", 
     "throwplayground", { name: "boyfriend test", path: "boyfriend test/index.html" }, 
-    "grapplinghook", "physicsandbox", "stacktower", "airhockey"
+    "grapplinghook", "physicsandbox", "stacktower", "airhockey", "chess"
 ];
 
 const fileList = document.getElementById('file-list');
