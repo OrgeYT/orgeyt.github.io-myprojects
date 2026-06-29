cb9d0781e9f2c24896c55d0fe6fa8017848047a0
OrgeYT <liamupper911@gmail.com>
2026-06-28 22:19:38 -0400

    Add new project '3d platformer turbowarp' to list

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ const projects = [
     { name: "COLOR MIX!", path: "color mix/index.html" }, "sadfunsandbox", { name: "My OC's lore", path: "loredrop.txt" }, "pongbutitsmadewithscratchblocks", "FCR",
     { name: "idk (By Brad)", path: "idk/index.html" }, { name: "magnet playground", path: "magnet/index.html" }, "batsurvival", "websimprojects", "wheelmaker", "popit", "sb3corrupter",
     "polygonprinter", "jsrunner", "rainbowparkour", { name: "First version of my website", path: "may20/index.html" }, { name: "what the", path: "index.html" }, "onlinebuilding", "piano", { name: "bounce and roll", path: "bounceandroll/index.html" },
-    "makeyourownai", "midisinger", "art", "svgtopng", "beastbrawl"
+    "makeyourownai", "midisinger", "art", "svgtopng", "beastbrawl", { name: "3d platformer turbowarp", path: "3dplatformerturbowarp/index.html" },
 ];
 
 const fileList = document.getElementById('file-list');
