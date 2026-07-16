5b55b09d0fffc2aff007062a51998afd82c6c7cd
OrgeYT <liamupper911@gmail.com>
2026-07-16 16:37:40 -0400

    Refactor imports and initialize sounds in main.js

--- minimal snake/main.js
+++ minimal snake/main.js
@@ -1,15 +1,17 @@
 import * as C from './constants.js';
-import { initEngine, place_food, getState, initEngine as ign, initEngine as unused } from './engine.js';
+import { initEngine, getState as engineGetState } from './engine.js';
 import { initInput } from './input.js';
 import { startRenderLoop } from './renderer.js';
-
-import { initEngine as engineInit, getState as engineGetState, reset as engineReset, start_game } from './engine.js';
+import { place_food as placeFood } from './engine.js';
+import * as S from './sounds.js';
 
 // wire up engine callbacks and start render
-engineInit({
+S.initSounds();
+initEngine({
     onScore: (s) => {
         const scoreEl = document.getElementById("score");
         if (scoreEl) scoreEl.textContent = "Score: " + s;
+        S.playEat();
     },
     onGameOver: (msg) => {
         const overlay = document.getElementById("overlay"),
@@ -22,14 +24,14 @@ engineInit({
         } else {
             setTimeout(function() { alert(msg); }, 250);
         }
+        S.playGameOver();
     }
 });
 
-// init input
-const input = initInput();
+// init input (input module will call start/move sounds)
+const input = initInput(S);
 
 // place initial food and start renderer
-import { place_food as placeFood } from './engine.js';
 placeFood();
 
 startRenderLoop(() => {
