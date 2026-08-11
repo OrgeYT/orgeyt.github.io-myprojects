4945d25faa814d4297368d1ba1ff474c942c0df0
OrgeYT <liamupper911@gmail.com>
2026-08-11 13:23:42 -0400

    Fix variable name and update gallery items

--- app.js
+++ app.js
@@ -733,7 +733,7 @@ const galleryCaption = document.getElementById('gallery-caption');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
-const GalleryItems = [
+const galleryItems = [
     { src: "Gallery/AAUGH.png", caption: '"Fun fact, this was actually the first file uploaded."' },
     { src: "Gallery/NOO.png", caption: '"NOO THEY CHANGED THE GOOGLE DRIVE LOGO"' },
     { src: "Gallery/avatar.png", caption: '"this is fine"' },
@@ -744,6 +744,7 @@ const GalleryItems = [
     { src: "Gallery/Untitled178.png", caption: "more art i made on my phone xd"},
     { src: "Gallery/ugh....png", caption: "ok so something just happened (jul 8), and it actually scared me. i was updating my site with gemini, until my css got nuked. idk what happened but something happened with the css. luckly github had back ups which i used."},
     { src: "Gallery/ABP.png", caption: "I played 'cats are liquid: a better place' and it was peak. i played the full story, all 12 worlds, on my phone. the lore was awesome, and the new machanics every new world was awesome. the game was a 10/10, i highly recommend the game if your bored or smt. im some what in the game's community now, so maybe expect some content about the game on my channel or smt. this game is also old, but still getting updates (i hope)"},
+    { src: "Gallery/ugh....png", caption: "Gemini decided to nuke the gallery lol but i fixed it"},
 ];
 
 let galleryIndex = 0;
@@ -782,9 +783,10 @@ let logoClickCount = 0;
 
 sidebarLogo.addEventListener('click', () => {
     logoClickCount++;
+
     if (logoClickCount >= 5) {
         unlockAchievement('control');
-        document.getElementById('dev-grid-modal').classList.remove('hidden');
+        window.location.href = "https://orgeyt.github.io/orgeyt.github.io-myprojects/secret_5Hd82K8Fb8.html";
         logoClickCount = 0;
     }
 });
