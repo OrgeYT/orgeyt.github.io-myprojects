51aeec72ab0f62ca47cb0df62f4de4123aea0b05
OrgeYT <liamupper911@gmail.com>
2026-09-01 06:41:36 -0400

    Revise comment for 'snake battle' project
    Updated comment for 'snake battle' project to reflect recent changes.

--- lists.js
+++ lists.js
@@ -150,7 +150,7 @@ const projects = [
     { name: "Classic Game", path: "classic games/index.html" }, // sonic, mario, kirby, wowwwwwwwwww!!!!! :3
     "platformer maker", // make some hard levels idc bro      |
     "ultimate tic tac toe", // look at the note at the bottom v
-    "snake battle" // yeah... im moving the projects to a projects folder tomorrow, its getting REEAL messy here.
+    "snake battle" // guys i moved the projects to a projects folder is that cool? i thought i would completely kill my website but i didnt.
 ];
 
 // ==========================================
