1c2c709017fbdd18dd1a90032558d07417758d7b
OrgeYT <liamupper911@gmail.com>
2026-09-03 09:10:51 -0400

    Fix syntax error in lists.js

--- lists.js
+++ lists.js
@@ -156,7 +156,8 @@ const projects = [
     "ultimate tic tac toe", // look at the note at the bottom v
     "snake battle", // guys i moved the projects to a projects folder is that cool? i thought i would completely kill my website but i didnt.
     "scanimation", // cool trick i guess. tricks your eyes into seeing a picture or animation.
-    "dodge the scams" // 150th project!!
+    "dodge the scams", // 150th project!!
+    "pixelartcreatorhtml" // yeah
 ];
 
 // ==========================================
